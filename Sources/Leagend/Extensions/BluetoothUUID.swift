d4b49f3a9a7dece1571121732489888ad38967b9
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-06 01:04:21 -0500

    Add `BluetoothUUID` extensions

---
+++ Sources/Leagend/Extensions/BluetoothUUID.swift
@@ -0,0 +1,16 @@
+//
+//  BluetoothUUID.swift
+//
+//
+//  Created by Alsey Coleman Miller on 3/6/24.
+//
+
+import Foundation
+import Bluetooth
+
+public extension BluetoothUUID {
+    
+    static var leagendBatteryVoltageCharacteristic: BluetoothUUID {
+        BluetoothUUID(rawValue: "0000FFF4-0000-1000-8000-00805F9B34FB")!
+    }
+}
