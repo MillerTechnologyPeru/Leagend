d6dc66128a1e6b9130f9e7679fdb6ef44c2637ea
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-08 01:47:23 -0500

    Add `LeagendAccessoryDetailView`

---
+++ Leagend.swiftpm/Views/AccessoryDetailView.swift
@@ -0,0 +1,457 @@
+//
+//  LeagendDetailView.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 4/12/23.
+//
+
+import Foundation
+import SwiftUI
+import Bluetooth
+import GATT
+import Leagend
+
+struct LeagendAccessoryDetailView: View {
+    
+    let peripheral: NativePeripheral
+    
+    @EnvironmentObject
+    private var store: AccessoryManager
+    
+    @State
+    private var reloadTask: Task<Void, Never>?
+    
+    @State
+    private var error: String?
+    
+    @State
+    private var isReloading = false
+    
+    @State
+    private var advertisement: LeagendAccessory.Advertisement?
+    
+    @State
+    private var information: Result<LeagendAccessoryInfo, Error>?
+    
+    init(
+        peripheral: NativePeripheral
+    ) {
+        self.peripheral = peripheral
+    }
+    
+    var body: some View {
+        VStack {
+            if let advertisement {
+                StateView(
+                    peripheral: peripheral,
+                    advertisement: advertisement,
+                    information: information
+                )
+            } else {
+                ProgressView()
+            }
+        }
+        .refreshable {
+            reload()
+        }
+        .onAppear {
+            reload()
+        }
+        .onDisappear {
+            reloadTask?.cancel()
+        }
+    }
+}
+
+extension LeagendAccessoryDetailView {
+    
+    func reload() {
+        // load advertisement
+        guard let advertisement = store.peripherals[peripheral] else {
+            // not longer in scan cache
+            return
+        }
+        self.advertisement = advertisement
+        // accessory metadata
+        if let accessoryInfo = store.accessoryInfo {
+            self.information = accessoryInfo[advertisement.type].flatMap { .success($0) }
+        } else {
+            // load accessory info
+            fetchAccessoryInfo()
+        }
+        // Bluetooth
+        //connect()
+    }
+    
+    func fetchAccessoryInfo() {
+        guard let advertisement else {
+            assertionFailure()
+            return
+        }
+        // networking and Bluetooth
+        let store = self.store
+        isReloading = true
+        reloadTask = Task(priority: .userInitiated) {
+            defer { isReloading = false }
+            do {
+                let accessoryInfo = try await store.downloadAccessoryInfo()
+                self.information = accessoryInfo[advertisement.type]
+                    .flatMap { .success($0) } ?? .failure(CocoaError(.coderValueNotFound))
+            }
+            catch {
+                self.information = .failure(error)
+            }
+        }
+    }
+    
+    func connect() {
+        guard let advertisement else {
+            assertionFailure()
+            return
+        }
+        Task {
+            do {
+                _ = try await store.connect(to: peripheral)
+            }
+            catch {
+                store.log("Unable to connect to \(peripheral). \(error)")
+            }
+        }
+    }
+    
+    func disconnect() {
+        guard let advertisement else {
+            return
+        }
+        Task {
+            await store.disconnect(peripheral)
+        }
+    }
+    
+    var isConnected: Bool {
+        store.connections.contains(peripheral)
+    }
+}
+
+extension LeagendAccessoryDetailView {
+    
+    struct StateView: View {
+        
+        let peripheral: NativePeripheral
+        
+        let advertisement: LeagendAccessory.Advertisement
+        
+        let information: Result<LeagendAccessoryInfo, Error>?
+        
+        var body: some View {
+            ScrollView {
+                VStack(spacing: 16) {
+                    // image view
+                    VStack {
+                        switch information {
+                        case .success(let success):
+                            CachedAsyncImage(
+                                url: URL(string: success.image),
+                                content: { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                }, placeholder: {
+                                    ProgressView()
+                                        .progressViewStyle(.circular)
+                                })
+                        case .failure(let failure):
+                            Image(systemName: "exclamationmark.triangle.fill")
+                                .foregroundColor(.yellow)
+                            Text(verbatim: failure.localizedDescription)
+                        case nil:
+                            ProgressView()
+                                .progressViewStyle(.circular)
+                        }
+                    }
+                    .frame(height: 250)
+                    .padding()
+                    
+                    // Actions
+                    switch advertisement.type {
+                    case .bm2:
+                        NavigationLink(destination: {
+                            VoltageView(peripheral: peripheral)
+                        }, label: {
+                            Text("Real-time Voltage")
+                        })
+                    }
+                    
+                    // Links
+                    if let information = try? information?.get() {
+                        if let manual = information.manual.flatMap({ URL(string: $0) }) {
+                            Link("User Manual", destination: manual)
+                        }
+                        if let website = information.website.flatMap({ URL(string: $0) }) {
+                            Link("Product Page", destination: website)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("\(advertisement.name)")
+        }
+    }
+}
+
+/*
+extension LeagendDetailView {
+    
+    func reload() {
+        let oldTask = reloadTask
+        reloadTask = Task {
+            self.error = nil
+            self.isReloading = true
+            defer { self.isReloading = false }
+            await oldTask?.value
+            do {
+                guard let beacons = store.peripherals[peripheral], beacons.isEmpty == false else {
+                    throw CentralError.unknownPeripheral
+                }
+                self.address = beacons.compactMapValues { $0.address }.values.first
+                self.capability = beacons.compactMap { $0.value.capability }.first ?? []
+                self.ioCapability = beacons.compactMap { $0.value.ioCapability }.first ?? []
+                // read characteristics
+                try await store.central.connection(for: peripheral) { connection in
+                    try await readCharacteristics(connection: connection)
+                }
+            }
+            catch {
+                self.error = error.localizedDescription
+            }
+        }
+    }
+    
+    func readCharacteristics(connection: GATTConnection<NativeCentral>) async throws {
+        var batteryService = ServiceSection(
+            id: .batteryService,
+            name: "Battery Service",
+            characteristics: []
+        )
+        
+        // read battery level
+        if let characteristic = connection.cache.characteristic(.batteryLevel, service: .batteryService) {
+            let data = try await connection.central.readValue(for: characteristic)
+            guard data.count == 1 else {
+                throw LeagendAppError.invalidCharacteristicValue(.batteryLevel)
+            }
+            let value = data[0]
+            batteryService.characteristics.append(
+                CharacteristicItem(
+                    id: characteristic.uuid.rawValue,
+                    name: "Battery Level",
+                    value: "\(value)%"
+                )
+            )
+        }
+        
+        // read temperature and humidity
+        var thermometerService = ServiceSection(
+            id: TemperatureHumidityCharacteristic.service,
+            name: "Mi Thermometer Service",
+            characteristics: []
+        )
+        if let characteristic = connection.cache.characteristic(TemperatureHumidityCharacteristic.uuid, service: TemperatureHumidityCharacteristic.service) {
+            let data = try await connection.central.readValue(for: characteristic)
+            guard let value = TemperatureHumidityCharacteristic(data: data) else {
+                throw LeagendAppError.invalidCharacteristicValue(TemperatureHumidityCharacteristic.uuid)
+            }
+            thermometerService.characteristics += [
+                CharacteristicItem(
+                    id: characteristic.uuid.rawValue + "-" + "Temperature",
+                    name: "Temperature",
+                    value: value.temperature.description
+                ),
+                CharacteristicItem(
+                    id: characteristic.uuid.rawValue + "-" + "Humidity",
+                    name: "Humidity",
+                    value: value.humidity.description
+                ),
+                CharacteristicItem(
+                    id: characteristic.uuid.rawValue + "-" + "BatteryVoltage",
+                    name: "Battery Voltage",
+                    value: value.batteryVoltage.description
+                )
+            ]
+        }
+        
+        // read device information
+        var deviceInformationService = ServiceSection(
+            id: .deviceInformation,
+            name: "Device Information",
+            characteristics: []
+        )
+        if let characteristic = connection.cache.characteristic(.manufacturerNameString, service: .deviceInformation) {
+            let data = try await connection.central.readValue(for: characteristic)
+            guard let value = String(data: data, encoding: .utf8) else {
+                throw LeagendAppError.invalidCharacteristicValue(.manufacturerNameString)
+            }
+            deviceInformationService.characteristics.append(
+                CharacteristicItem(
+                    id: BluetoothUUID.manufacturerNameString.rawValue,
+                    name: "Manufacturer Name",
+                    value: value
+                )
+            )
+        }
+        if let characteristic = connection.cache.characteristic(.modelNumberString, service: .deviceInformation) {
+            let data = try await connection.central.readValue(for: characteristic)
+            guard let value = String(data: data, encoding: .utf8) else {
+                throw LeagendAppError.invalidCharacteristicValue(.modelNumberString)
+            }
+            deviceInformationService.characteristics.append(
+                CharacteristicItem(
+                    id: BluetoothUUID.modelNumberString.rawValue,
+                    name: "Model",
+                    value: value
+                )
+            )
+        }
+        if let characteristic = connection.cache.characteristic(.serialNumberString, service: .deviceInformation) {
+            let data = try await connection.central.readValue(for: characteristic)
+            guard let value = String(data: data, encoding: .utf8) else {
+                throw LeagendAppError.invalidCharacteristicValue(.serialNumberString)
+            }
+            deviceInformationService.characteristics.append(
+                CharacteristicItem(
+                    id: BluetoothUUID.serialNumberString.rawValue,
+                    name: "Serial Number",
+                    value: value
+                )
+            )
+        }
+        if let characteristic = connection.cache.characteristic(.firmwareRevisionString, service: .deviceInformation) {
+            let data = try await connection.central.readValue(for: characteristic)
+            guard let value = String(data: data, encoding: .utf8) else {
+                throw LeagendAppError.invalidCharacteristicValue(.firmwareRevisionString)
+            }
+            deviceInformationService.characteristics.append(
+                CharacteristicItem(
+                    id: BluetoothUUID.firmwareRevisionString.rawValue,
+                    name: "Firmware Revision",
+                    value: value
+                )
+            )
+        }
+        if let characteristic = connection.cache.characteristic(.hardwareRevisionString, service: .deviceInformation) {
+            let data = try await connection.central.readValue(for: characteristic)
+            guard let value = String(data: data, encoding: .utf8) else {
+                throw LeagendAppError.invalidCharacteristicValue(.hardwareRevisionString)
+            }
+            deviceInformationService.characteristics.append(
+                CharacteristicItem(
+                    id: BluetoothUUID.hardwareRevisionString.rawValue,
+                    name: "Hardware Revision",
+                    value: value
+                )
+            )
+        }
+        if let characteristic = connection.cache.characteristic(.softwareRevisionString, service: .deviceInformation) {
+            let data = try await connection.central.readValue(for: characteristic)
+            guard let value = String(data: data, encoding: .utf8) else {
+                throw LeagendAppError.invalidCharacteristicValue(.softwareRevisionString)
+            }
+            deviceInformationService.characteristics.append(
+                CharacteristicItem(
+                    id: BluetoothUUID.softwareRevisionString.rawValue,
+                    name: "Software Revision",
+                    value: value
+                )
+            )
+        }
+        
+        // set services
+        self.services = [
+            thermometerService,
+            batteryService,
+            deviceInformationService
+        ]
+        .filter { $0.characteristics.isEmpty == false }
+    }
+}
+
+extension LeagendDetailView {
+    
+    struct StateView: View {
+        
+        let product: ProductID
+        
+        let address: BluetoothAddress?
+        
+        let version: UInt8
+        
+        let capability: Leagend.Capability
+        
+        let ioCapability: Leagend.Capability.IO
+        
+        let services: [ServiceSection]
+        
+        var body: some View {
+            List {
+                Section("Advertisement") {
+                    if let address = self.address {
+                        SubtitleRow(
+                            title: Text("Address"),
+                            subtitle: Text(verbatim: address.rawValue)
+                        )
+                    }
+                    SubtitleRow(
+                        title: Text("Version"),
+                        subtitle: Text(verbatim: version.description)
+                    )
+                    #if DEBUG
+                    if capability.isEmpty == false {
+                        SubtitleRow(
+                            title: Text("Capability"),
+                            subtitle: Text(verbatim: capability.description)
+                        )
+                    }
+                    if ioCapability.isEmpty == false {
+                        SubtitleRow(
+                            title: Text("IO Capability"),
+                            subtitle: Text(verbatim: ioCapability.description)
+                        )
+                    }
+                    #endif
+                }
+                ForEach(services) { service in
+                    Section(service.name) {
+                        ForEach(service.characteristics) { characteristic in
+                            SubtitleRow(
+                                title: Text(characteristic.name),
+                                subtitle: Text(verbatim: characteristic.value)
+                            )
+                        }
+                    }
+                }
+            }
+            .navigationTitle("\(product.description)")
+        }
+    }
+}
+
+extension LeagendDetailView {
+    
+    struct ServiceSection: Equatable, Identifiable {
+        
+        let id: BluetoothUUID
+        
+        let name: LocalizedStringKey
+        
+        var characteristics: [CharacteristicItem]
+    }
+    
+    struct CharacteristicItem: Equatable, Identifiable {
+        
+        let id: String
+        
+        let name: LocalizedStringKey
+        
+        let value: String
+    }
+}
+*/
